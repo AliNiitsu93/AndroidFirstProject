9439bf4d3555111840c3d1656e8b556da27eb971
AliNiitsu93 <ali-abbas.niitsu@mnsu.edu>
2020-11-23 21:03:53 -0600

    practice 4
    Testing return btn function

--- Practice4/app/src/main/java/com/example/practice4/ShoppingCart.kt
+++ Practice4/app/src/main/java/com/example/practice4/ShoppingCart.kt
@@ -1,11 +1,22 @@
 package com.example.practice4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_shopping_cart.*
 
 class ShoppingCart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_cart)
+
+
+
+        btnReturn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            //start the next activity
+            startActivity(intent)
+        }
     }
 }
