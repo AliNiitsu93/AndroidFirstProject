d628bfef54890e44e8bb79a82f74538dc4eab284
AliNiitsu93 <ali-abbas.niitsu@mnsu.edu>
2020-11-23 20:42:41 -0600

    Practice4
    create multi-activity

--- Practice2/app/src/main/java/com/example/practice2/MyDBHelper.kt
+++ Practice2/app/src/main/java/com/example/practice2/MyDBHelper.kt
@@ -6,7 +6,11 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class MyDBHelper (context: Context): SQLiteOpenHelper(context, "USERDB", null, 1 ) {
     override fun onCreate(db: SQLiteDatabase?) {
+        // Create a Table called USERS, where USERID is an integer an autoincrement.
         db?.execSQL("CREATE TABLE USERS(USERID INTEGER PRIMARY KEY AUTOINCREMENT, UNAME TEXT, PWD TEXT)")
+
+
+        //just some data added as default input
         db?.execSQL("INSERT INTO USERS(UNAME,PWD) VALUES('Ali@mnsu.edu','pcs')")
         db?.execSQL("INSERT INTO USERS(UNAME,PWD) VALUES('Ali@gmail.com','ATO')")
     }
---
+++ Practice4/app/src/main/java/com/example/practice4/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.practice4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnStartShopping.setOnClickListener {
+            val intent = Intent(this, ShoppingCart::class.java)
+            //start the next activity
+            startActivity(intent)
+        }
+
+    }
+}
